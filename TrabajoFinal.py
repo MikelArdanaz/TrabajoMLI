97d9e991cdc8c57c6da09e92fc253e4e8c44229a
MikelArdanaz <mikelillo_1@msn.com>
2019-01-05 20:57:55 +0100

    Second approach to Kmeans: Classify only labeled and keep the unlabeled

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -1,7 +1,5 @@
-'''
-Trabajo Machine Learning
-@authors: Vicente Cifre, Mikel Ardanaz
-'''
+# Trabajo Machine Learning
+# @authors: Vicente Cifre, Mikel Ardanaz
 import numpy as np
 import scipy.io.matlab as matlab
 import matplotlib.pyplot as plt
@@ -10,18 +8,18 @@ from sklearn.cluster import KMeans
 
 # TODO tener todos los modelos en un mismo diccionario llamado modelos con string modeloparametros y modelo
 
-def Kmeans(imagen):
+def kmeans(imagen):
     '''
-    Realiza un clustering utilizando el algoritmo KMeans implementado en scikit-learn con 5,10y 20 clusters.
+    Realiza un clustering utilizando el algoritmo KMeans implementado en scikit-learn con 5,10 y 17 clusters.
     :return: * modelos, predictions: El modelo ya entrenado y el resultado de la prediccion
     '''
     modelos=[]
     predictions=[]
-    for nclusters in (5,10,20):
+    for nclusters in (5,10,17):# Yl va de 0 a 16
         kmeans = KMeans(n_clusters=nclusters, random_state=42).fit(imagen)
         modelos.append(kmeans)
-        predictions.append(kmeans.predict(imagen))
-    return modelos,predictions
+        predictions.append(kmeans.predict(imagen))# Prescindible (https://stackoverflow.com/questions/25012342/scikit-learns-k-means-what-does-the-predict-method-really-do)
+    return modelos, predictions
 
 
 if __name__ == '__main__':
@@ -41,10 +39,21 @@ if __name__ == '__main__':
     # Reshape del Ground Truth como una imagen
     Y = np.reshape(Yl, (X.shape[0], X.shape[1]),order="F")
     imagen=np.float64(Xl[:,(range(0,220))])#imagen en sus 220 dimensiones
-    modelos,predictions=Kmeans(imagen)
+    # 1º Aproximación: Kmeans 5,10,17 clusters
+    modelos, predictions= kmeans(imagen)
     for i in range(3):
         ax = plt.subplot(1, 3, i+1)
-        ax.imshow(predictions[i].reshape((145, 145), order="F")), plt.title('K-means clustering ')
+        ax.imshow(predictions[i].reshape((145, 145), order="F"))
+        plt.title('K-means '+str(modelos[i].n_clusters)+' clusters') # Dims(Y)
+    plt.show()
+    # 2º Aprox: Probamos ahora a hacer cluster solo de los que tienen etiqueta (al resto les mantendremos el valor de 0)
+    Y_final_orig = np.zeros((Yl.shape[0], 1))
+    Xl_SoloClasei = np.float64(Xl[Yl != 0, :])
+    modelos1 = KMeans(n_clusters=16, random_state=42).fit(Xl_SoloClasei)
+    Y_final_orig[Yl != 0, 0] = modelos1.labels_ + 1 # Kmeans empieza a etiquetar en 0
+    plt.imshow(Y_final_orig.reshape((145, 145), order="F"))
+    plt.title('K-means 16 clusters, solo etiquetados')
+    plt.show()
 
     # Filter background: eliminamos la clase 0 de los datos etiquetados
     # Nc=Yl.max()-Yl.min()+1
@@ -58,3 +67,12 @@ if __name__ == '__main__':
     ax.imshow(X[:,:,1]), ax.axis('off'), plt.title('Image')
     ax=plt.subplot(1,2,2)
     ax.imshow(Y), ax.axis('off'), plt.title('Ground Truth')
+
+    # Dibujamos los resultados
+    clasmap = Y;  # aqui deberiamos poner nuestra clasificacion
+    clasmap_masked = np.ma.masked_where(clasmap < 1, clasmap)
+    # for i in range(15):
+    # plt.imshow(X[:,:,1])
+
+    plt.imshow(clasmap_masked)
+    plt.show()
