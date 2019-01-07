e9c24ed301e45a6b4c8507f14cbd3526f9d0d2d6
MikelArdanaz <mikelillo_1@msn.com>
2019-01-07 11:40:46 +0100

    Implemented PCA approach. Modularization

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -5,27 +5,38 @@ import scipy.io.matlab as matlab
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.decomposition import PCA
 
 # TODO tener todos los modelos en un mismo diccionario llamado modelos con string modeloparametros y modelo
-# TODO modularizar
 # TODO testear jerarquico
 
 
-def kmeans(imagen):
+def kmeans(imagen,tipo='',labeled=False):
     '''
     Realiza un clustering utilizando el algoritmo KMeans implementado en scikit-learn con 5,10 y 17 clusters.
     :return: * modelos, predictions: El modelo ya entrenado y el resultado de la prediccion
     '''
     modelos=[]
     predictions=[]
-    for nclusters in (5,10,17):# Yl va de 0 a 16
+    for i,nclusters in enumerate([5,10,17]):# Yl va de 0 a 16
         kmeans = KMeans(n_clusters=nclusters, random_state=42,n_jobs=-1).fit(imagen)# njobs=1-> Parallel processing
         modelos.append(kmeans)
         predictions.append(kmeans.predict(imagen))# Prescindible (https://stackoverflow.com/questions/25012342/scikit-learns-k-means-what-does-the-predict-method-really-do)
+        ax = plt.subplot(1, 3, i+1)
+        plt.suptitle('Kmeans')
+        if labeled:
+            Y_final_orig[Yl != 0, 0] = modelos[i].labels_ + 1
+            predictions[i]=Y_final_orig
+        ax.imshow(predictions[i].reshape((145, 145), order="F"))
+        plt.title(tipo+str(modelos[i].n_clusters)+' clusters') # Dims(Y)
+    plt.show()
     return modelos, predictions
-
-
+def PCApply(X):
+    data=PCA(n_components=40).fit_transform(X)
+    _, b,_ = np.linalg.svd(X.transpose().dot(X))# Demo mejor con 40
+    plt.plot(range(10, 75), b[10:75], 'bx-')
+    plt.show()
+    return data
 if __name__ == '__main__':
     # Lectura de la imagen de fichero de Matlab .mat
     mat_file ="datasetB1.mat"
@@ -45,11 +56,6 @@ if __name__ == '__main__':
     imagen=np.float64(Xl[:,(range(0,220))])#imagen en sus 220 dimensiones
     # 1º Aproximación: Kmeans 5,10,17 clusters
     modelos, predictions= kmeans(imagen)
-    for i in range(3):
-        ax = plt.subplot(1, 3, i+1)
-        ax.imshow(predictions[i].reshape((145, 145), order="F"))
-        plt.title('K-means '+str(modelos[i].n_clusters)+' clusters') # Dims(Y)
-    plt.show()
     # 2º Aprox: Filter background
     # Probamos ahora a hacer cluster solo de los que tienen etiqueta (al resto les mantendremos el valor de 0)
     Y_final_orig = np.zeros((Yl.shape[0], 1))
@@ -63,19 +69,16 @@ if __name__ == '__main__':
     stander = StandardScaler()
     Xl_std = stander.fit_transform(Xl)
     imagen = np.float64(Xl_std[:, (range(0, 220))])
-    modelos, predictions = kmeans(imagen)
-    for i in range(3):
-        ax = plt.subplot(1, 3, i + 1)
-        ax.imshow(predictions[i].reshape((145, 145), order="F"))
-        plt.title('K-means std ' + str(modelos[i].n_clusters) + ' clusters')  # Dims(Y)
-    plt.show()
+    modelos, predictions = kmeans(imagen,'std')
     Xl_stdclase = np.float64(Xl_std[Yl != 0, :])
     modelos1 = KMeans(n_clusters=16, random_state=42).fit(Xl_stdclase)
     Y_final_orig[Yl != 0, 0] = modelos1.labels_ + 1  # Kmeans empieza a etiquetar en 0
     plt.imshow(Y_final_orig.reshape((145, 145), order="F"))
     plt.title('K-means std 16 clusters, etiquetados')
     plt.show()
-
+    #4º Aprox: PCA
+    data=PCApply(Xl_std)
+    modelosPCA, predictionsPCA=kmeans(data[Yl!=0,:],'PCA + std ',labeled=True)
     # Dibujamos las imagenes
     ax=plt.subplot(1,2,1)
     ax.imshow(X[:,:,1]), ax.axis('off'), plt.title('Image')
